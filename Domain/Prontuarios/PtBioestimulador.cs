7220ce699b8751e2f7ed5823d8190c4e9308f2b2
walace <walacehelker123@gmail.com>
2025-09-29 21:07:39 -0300

    Adicionada funcionalidade para preencher campo de total utilizaado de forma automática

---
+++ Domain/Prontuarios/PtBioestimulador.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Base;
+using Domain.Cadastro;
+
+namespace Domain.Prontuarios
+{
+  public class PtBioestimulador : BaseDomainEntity
+  {
+    public Guid PessoaId { get; set; }
+    public DateTime DataProcedimento { get; set; }
+    
+    public byte[] PdfAssinado { get; set; }
+    public CadPessoa CadPessoa { get; set; }
+  }
+}
